998a4b01bb62e74873f657a3b051ca1a278facc0
gallardojpsistemas <gallardojpsistemas@gmail.com>
2022-08-14 10:02:25 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script>
+  import Logo from "./assets/logo.svg"
+</script>
+
+<template>
+  <img src="./assets/logo.svg" alt="Logo Vue">
+  <h1>Template es una etiqueta HTML, permite ejecutar adentro codigo html</h1>
+  <p>SFC (Single File Component)
+           Single File Components(SFCs) are a style of application organization used by JavaScript UI libraries where each file represents a single component in all aspects. Typically they resemble an HTML document where you have HTML tags, Style Tag, and Script Tag all in a file. This is the common pattern for UI Frameworks like Vue and Svelte.</p>
+</template>
